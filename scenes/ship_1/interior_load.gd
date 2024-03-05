d0d27d266ab35ee3d32aedb89eee6db2e15bc8e0
Amathou <133946636+Amathou@users.noreply.github.com>
2024-03-04 17:59:15 -0800

    scene loading changes
    added stuff to the main test scene (test loading zones) and am now using a new way to load the scenes cause the way i was doin it was throwing errors

---
+++ scenes/ship_1/interior_load.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+var entered = false
+
+func _on_body_entered(_body: PhysicsBody2D):
+	entered = true
+
+
+func _on_body_exited(_body):
+	entered = false
+
+
+func _process(_delta):
+	if entered == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://scenes/ship_1/Interior/interior.tscn")
